22cbaff38c633f3a5b664ae4a80b646d2a8b0997
DavidFMarins <davidfelipemarins@gmail.com>
2023-04-24 20:21:44 -0300

    metodos

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,16 @@
+public class Operadores {
+    
+    public static void main(String[] args) {
+       int numero1 = 1;
+       int numero2 = 2;
+       
+       boolean simNao = numero1 == numero2;
+
+       System.out.println("numeroUm é igual a numeroDois ?" + simNão);
+
+       simNao = numero1 != numero2;
+
+       System.out.println("nuemroUm ");
+        
+    }
+}
